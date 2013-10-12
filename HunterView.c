b304a53e7e1e8f780d0855a73bb2e7c885266d61
Neil Dela Cruz <neil.d.delacruz@gmail.com>
2013-10-12 18:35:02 +1100

    started working on getHistory function

--- HunterView.c
+++ HunterView.c
@@ -6,6 +6,8 @@
 #include <string.h>
 
 #define PLAYLEN 7
+
+static LocationID translateLocationID(char*);
      
 struct hunterView {
     //REPLACE THIS WITH YOUR OWN IMPLEMENTATION
@@ -133,4 +135,59 @@ void disposeHunterView( HunterView toBeDeleted ) {
 //Get the current round
 Round getRound (HunterView currentView) {
     return currentView->totalTurns/5;
+}
+
+// Fills the trail array with the location ids of the last 6 turns for the given player
+// For dracula this may include integers:
+//   in the interval [0...70] if Dracula was known to be in a city or sea
+//   CITY_UNKNOWN     if Dracula was in an unknown city
+//   SEA_UNKNOWN      if Dracula was in an unknown sea
+
+//   HIDE             if Dracula was known to have made a hide move
+//   DOUBLE_BACK_N    where N is [0...5], if Dracula was known to have
+//                    made a double back move N positions back in the trail
+//                    e.g. DOUBLE_BACK_1 is the last place place he visited
+//   TELEPORT         if Dracula apparated back to Castle Dracula
+
+
+// For any player if the move does not exist yet (i.e, the start of the game),
+// the value should be UNKNOWN_LOCATION (-1)
+// For example after 2 turns the array may have the contents
+// {29, 182, -1, -1, -1, -1} 
+// This would mean in the first move the player started on location 182 
+// then moved to the current location of 29
+void getHistory (HunterView currentView, PlayerID player,LocationID trail[TRAIL_SIZE]) {
+    int i;
+    char location[3];
+    location[2] = '\0';
+
+    // Fill array with -1
+    for (i=0; i<TRAIL_SIZE; i++) {
+        trail[i] = -1;
+    }
+
+    Round currentRound = getRound(currentView);
+    printf("totalround = %d\n", currentRound);
+
+    if (currentRound > 0) {
+        for(i=0; i < getRound(currentView); i++) {
+            printf("round = %d, playerid = %d\n", i, player);
+            location[0] = currentView->seperatedPP[(i*5)+player][1];
+            location[1] = currentView->seperatedPP[(i*5)+player][2];
+            trail[i] = translateLocationID(location);
+            printf("location = **%s**\n", location);
+        }
+        if (currentView->totalTurns%5 > player) {
+            printf("round = %d, playerid = %d\n", i, player);
+            location[0] = currentView->seperatedPP[(i*5)+player][1];
+            location[1] = currentView->seperatedPP[(i*5)+player][2];
+            trail[i] = translateLocationID(location);
+            printf("location = **%s**\n", location);
+        }
+    }
+
+    for (i=0; i<TRAIL_SIZE; i++) {
+        printf("history [%d] = %d\n", i, trail[i]);
+    }
+
 }
