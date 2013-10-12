23bdd9348ff9153cb1e69b41ad6399361b6b094c
Neil Dela Cruz <neil.d.delacruz@gmail.com>
2013-10-12 12:34:16 +1100

    finished getCurrentPlayer function

--- HunterView.c
+++ HunterView.c
@@ -14,22 +14,34 @@ struct hunterView {
     // stores the total amount of turns that have occured within the game
     int totalTurns; 
     // all the pastPlays(PP) seperated, so that they can be individually accessed through the array
-    char seperatedPP[totalTurns][PLAYLEN+1]; 
+    char** seperatedPP;
+    
 };
-     
+
 //SPLIT STRING PLAY INTO 2D array with 7 chars. 
 HunterView newHunterView( char *pastPlays, playerMessage messages[] ) {
     HunterView hunterView = malloc( sizeof( *hunterView ) );
     hunterView->score = GAME_START_SCORE;
     hunterView->player = PLAYER_LORD_GODALMING;
-    
-    int i;
+
+    int i,k;
     int counter;
     
     counter = 0;
     hunterView->totalTurns = strlen(pastPlays)/PLAYLEN;
     
-    
+    // Initialise the 2D array of strings
+    // http://stackoverflow.com/a/14583642
+    // Initialise an array of pointers for the  amount of total turns
+    hunterView->seperatedPP = malloc (hunterView->totalTurns * sizeof(char*));
+    assert(hunterView->seperatedPP != NULL);
+
+    // Intialise a string for every turn
+    for(k = 0; k < hunterView->totalTurns; k++) {
+        hunterView->seperatedPP[k] = malloc((PLAYLEN+1)*sizeof(char));
+        assert(hunterView->seperatedPP[k] != NULL);
+    }
+
     for(i=0; i<hunterView->totalTurns; i++){
 
         int j;
@@ -50,7 +62,7 @@ HunterView newHunterView( char *pastPlays, playerMessage messages[] ) {
     //hunterView->score = calculateScore(finalPlay);
     return hunterView;
 }
-     
+
 //static int calculateScore (finalPlay);
 
 //Get the current score
@@ -70,9 +82,20 @@ int getScore(HunterView currentView){
 //   DRACULA        (4): Dracula's turn
 PlayerID getCurrentPlayer (HunterView currentView){
 
-    PlayerID player;
-    player = currentView -> player;
-    return player;
+    PlayerID currentPlayer = PLAYER_LORD_GODALMING;
+    char currentPlayer = currentView->seperatedPP[0][currentView->totalTurns-1];
+    //printf("currentPlayer = *%c*\n", currentPlayer);
+    if (currentPlayer == 'G') {
+        return PLAYER_SEWARD;
+    } else if (currentPlayer == 'S') {
+        return PLAYER_VAN_HELSING;
+    } else if (currentPlayer == 'H') {
+        return PLAYER_MINA_HAKER;
+    } else if (currentPlayer == 'M') {
+        return PLAYER_DRACULA;
+    } else if (currentPlayer == 'D') {
+        return PLAYER_LORD_GODALMING;
+    }
 }
      
 // this function frees all memory previously allocated for the HunterView
